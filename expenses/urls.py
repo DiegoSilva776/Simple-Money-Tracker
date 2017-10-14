b374a12f9013ce8b84953fc62b2a7b37afdaa736
Diego Marcolino Silva <diego.marcolino.silva@gmail.com>
2017-10-14 06:34:20 -0300

    Refactored the views of the expenses app with Viewsets and refactored the urls of the expenses app with the Router class

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,46 +3,18 @@ from django.conf.urls import include
 
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 
-from expenses.views import ExpenseViewSet, UserViewSet, api_root
+from expenses import views
 
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'expenses', views.ExpenseViewSet)
+router.register(r'users', views.UserViewSet)
 
-# Explicitly create the ViewSets
-expense_list = ExpenseViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-expense_detail = ExpenseViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-expense_highlight = ExpenseViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
-
-
-# Allow passing the content type at the end of the Request url and name all endpoints but the root
-urlpatterns = format_suffix_patterns([
-    url(r'^$', api_root),
-    url(r'^expenses/$', expense_list, name='expense-list'),
-    url(r'^expenses/(?P<pk>[0-9]+)/$', expense_detail, name='expense-detail'),
-    url(r'^expenses/(?P<pk>[0-9]+)/highlight/$', expense_highlight, name='expense-highlight'),
-    url(r'^users/$', user_list, name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', user_detail, name='user-detail'),
-])
-
-# Add the default login and logout endpoints from the DjangoREST framework to the app
-# The r'^api-auth/' part of pattern can actually be whatever URL you want to use. 
-# The only restriction is that the included urls must use the 'rest_framework' namespace.
-urlpatterns += [
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+# The API URLs are now determined automatically by the router.
+# Additionally, we include the login URLs for the browsable API.
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
